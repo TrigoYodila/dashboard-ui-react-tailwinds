353db8fb86d82b4859454268b42351821d6bed9f
Trigo <trigoyodila1996@gmail.com>
2024-01-12 15:17:33 +0100

    add logout btn

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -3,6 +3,7 @@ import ImageLogo from '../../assets/logo.png'
 import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/constants'
 import { Link, useLocation } from 'react-router-dom'
 import classNames from 'classnames'
+import { HiOutlineLogout } from 'react-icons/hi'
 
 const linkClass = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
@@ -27,6 +28,12 @@ function Sidebar() {
                         <SideBarLink key={item.key} item={item}/>
                     ))
                 }
+                <div className={classNames(linkClass, 'cursor-pointer text-red-500')}>
+					<span className="text-xl">
+						<HiOutlineLogout />
+					</span>
+					Logout
+				</div>
             </div>
         </div>
     )
